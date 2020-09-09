d1a0e3dd3ac9e6136bcb0ee280aaa963bdff3d66
LEE-JANGWO <hsrmmg10@gmail.com>
2020-09-09 14:17:51 +0900

    최종수정본

--- src/routes/About.js
+++ src/routes/About.js
@@ -8,7 +8,7 @@ function About(props) {
             <span>
                 리액트 첫번째 토이프로젝트 영화 평점 웹사이트 
             </span>
-            <span></span>
+            <span>https://github.com/LEE-JANGWO</span>
         </div>
     );
 }
